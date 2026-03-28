d2cf6656054809fd8b9997636e3712af3f21d485
Joao <jpgaldinoesilva@gmail.com>
2026-03-28 14:12:28 -0300

    terceiro commit teste

--- projeto/ToDoList/ToDo/Program.cs
+++ projeto/ToDoList/ToDo/Program.cs
@@ -1,3 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Olá, mundo!");
+Console.WriteLine("Terceiro teste commit!");
--- projeto/ToDoList/ToDo/obj/Debug/net8.0/ToDo.AssemblyInfo.cs
+++ projeto/ToDoList/ToDo/obj/Debug/net8.0/ToDo.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ToDo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c7ef27d0df6ddd7eab517ecd3c2a08c4a5b2797b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3c3b8f12fea36869e9923b147bc61a62e84caedc")]
 [assembly: System.Reflection.AssemblyProductAttribute("ToDo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ToDo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
